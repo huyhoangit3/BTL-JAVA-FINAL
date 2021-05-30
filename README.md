c511bff4cb476c191f6cc4aac8941723e4ea24e2
Lường Bá Hoàng <hoang2kthptlk@gmail.com>
2021-05-30 12:54:44 +0700

    commited at 30/05 on Windows

---
+++ README.md
@@ -0,0 +1,11 @@
+Requirement to run this project
+ - Intellij IDE Community Edition. Link download: https://www.jetbrains.com/idea/download/#section=windows
+ - JDK 8 or later. Link download: https://www.oracle.com/java/technologies/javase/javase-jdk8-downloads.html
+ - Python 3 interpreter. Link download: https://www.python.org/downloads/
+ - matplotlib and numpy library for python.
+
+How to install matplotlib and numpy modules:
+ - Make sure python 3 version installed on computer.
+ - Open command prompt on Windows and type:
+    python -m pip install numpy
+    python -m pip install matplotlib
--- src/com/nhom7/controller/MainFormController.java
+++ src/com/nhom7/controller/MainFormController.java
@@ -62,7 +62,7 @@ public class MainFormController {
 
     public void callOtherStage() {
         FXMLLoader loader = new FXMLLoader(Objects.requireNonNull(getClass().
-                getResource("../view/infoForm.fxml")));
+                getResource("..\\view\\infoForm.fxml")));
         try {
             Parent root = loader.load();
             infoFormController = loader.getController();
@@ -99,7 +99,7 @@ public class MainFormController {
         Stage stage = (Stage) ap.getScene().getWindow();
         FileChooser fc = new FileChooser();
         fc.setTitle("Choose a data set");
-        fc.setInitialDirectory(new File(System.getProperty("user.dir"), "src/com/nhom7/datasets"));
+        fc.setInitialDirectory(new File(System.getProperty("user.dir"), "src\\com\\nhom7\\datasets"));
         File file = fc.showOpenDialog(stage);
         if (file != null) {
             String fullPath = file.getAbsolutePath();
